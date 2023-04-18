447733327da824f9203e39e57ca3b5a908cd230b
Giacomo Bosio <30754884+giacomobosio@users.noreply.github.com>
2023-04-18 18:29:17 +0200

    Quite all info added

--- data/components.tsx
+++ data/components.tsx
@@ -1,5 +1,5 @@
 import { AiFillInstagram } from "react-icons/ai";
-import { FaSnapchatGhost } from "react-icons/fa";
+import { FaSnapchatGhost, FaPodcast, FaRegCalendarAlt } from "react-icons/fa";
 import { HiGlobeAlt, HiViewGrid, HiBookOpen } from "react-icons/hi";
 import { MdAttachMoney } from "react-icons/md";
 import { RiGithubFill, RiLinkedinBoxFill, RiFacebookBoxFill, RiTwitterFill, RiTelegramFill, RiAtFill } from "react-icons/ri";
@@ -12,6 +12,8 @@ export default {
   MdAttachMoney,
   AiFillInstagram,
   FaSnapchatGhost,
+  FaPodcast,
+  FaRegCalendarAlt,
   RiLinkedinBoxFill,
   RiFacebookBoxFill,
   RiTwitterFill,
