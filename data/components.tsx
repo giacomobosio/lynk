3af4c54d24dedb895737b70ef3f5dd927b8176a3
Giacomo Bosio <30754884+giacomobosio@users.noreply.github.com>
2024-11-17 09:52:06 +0100

    Added X and Bluesky

--- data/components.tsx
+++ data/components.tsx
@@ -2,7 +2,8 @@ import { AiFillInstagram } from "react-icons/ai";
 import { FaSnapchatGhost, FaPodcast, FaRegCalendarAlt } from "react-icons/fa";
 import { HiGlobeAlt, HiViewGrid, HiBookOpen } from "react-icons/hi";
 import { MdAttachMoney } from "react-icons/md";
-import { RiGithubFill, RiLinkedinBoxFill, RiFacebookBoxFill, RiTwitterFill, RiTelegramFill, RiAtFill } from "react-icons/ri";
+import { RiGithubFill, RiLinkedinBoxFill, RiFacebookBoxFill, RiTwitterFill, RiTelegramFill, RiAtFill, RiBlueskyFill } from "react-icons/ri";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default {
   HiGlobeAlt,
@@ -19,4 +20,6 @@ export default {
   RiTwitterFill,
   RiTelegramFill,
   RiAtFill,
+  RiBlueskyFill,
+  FaXTwitter,
 };
